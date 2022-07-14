b0ec2e7f45d700ef95a5a9fdc64e818c82da4e1a
srevanwar09 <srevanwar09@domain.com>
2022-07-14 23:45:17 +0530

    Program.cs file added in UC-2 branch

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -1,11 +1,23 @@
-int Is_FULL_TIME = 1;
-Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == Is_FULL_TIME)
+class Program
 {
-    Console.WriteLine("Employee is Present");
-}
-else
-{
-    Console.WriteLine("Employee is Absent");
+    static void Main(string[] args)
+    {
+        int Is_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == Is_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : "+empWage);
+    }
 }
