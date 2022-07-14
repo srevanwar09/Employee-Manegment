b7a56ced38ba029cf096b18791a57b4735bb164a
srevanwar09 <srevanwar09@domain.com>
2022-07-15 00:28:35 +0530

    Program.cs file added in UC-5 branch

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -3,28 +3,34 @@ class Program
     public const int IS_PART_TIME = 1;
     public const int Is_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
+    public const int  NUM_OF_WORKING_DAYS= 2;
     static void Main(string[] args)
     {
         int empHrs = 0;
         int empWage = 0;
-        Random random = new Random();
-
-        int empCheck = random.Next(0, 3);
-        switch (empCheck)
+        int totalEmpWage=0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
         {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
 
-            case Is_FULL_TIME:
-                empHrs = 8;
-                break;
- 
-            default:
-                empHrs = 0;
-                break;
+                case Is_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage : "+empWage);
+        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
     }
 }
