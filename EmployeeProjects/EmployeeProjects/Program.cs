07d973c5e60e55b91c4c8a194c9e164c19ca22c4
srevanwar09 <srevanwar09@domain.com>
2022-07-15 00:00:36 +0530

    Program.cs file added in UC-3 branch

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -2,14 +2,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        int Is_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int Is_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
         Random random = new Random();
 
-        int empCheck = random.Next(0, 2);
-        if (empCheck == Is_FULL_TIME)
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
+        }
+        else if(empCheck==Is_FULL_TIME)
         {
             empHrs = 8;
         }
