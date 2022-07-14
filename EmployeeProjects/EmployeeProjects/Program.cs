bc2f613f0aca1e80f4d548b1c8e14e0b14cd1d7f
srevanwar09 <srevanwar09@domain.com>
2022-07-14 22:54:20 +0530

    UC-1 are added in file

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 int Is_FULL_TIME = 1;
-=======
-﻿int Is_FULL_TIME = 1;
->>>>>>> UC-1
 Random random = new Random();
 int empCheck = random.Next(0, 2);
 if (empCheck == Is_FULL_TIME)
 {
     Console.WriteLine("Employee is Present");
-<<<<<<< HEAD
-        }
-=======
 }
->>>>>>> UC-1
 else
 {
     Console.WriteLine("Employee is Absent");
