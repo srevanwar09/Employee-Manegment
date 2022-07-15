4b79c8c5937b91661fb2893776186df03cb46079
srevanwar09 <srevanwar09@domain.com>
2022-07-15 22:04:27 +0530

    program are added in uc-7

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -3,13 +3,13 @@ class Program
     public const int IS_PART_TIME = 1;
     public const int Is_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
-    public const int  NUM_OF_WORKING_DAYS = 2;
+    public const int NUM_OF_WORKING_DAYS = 2;
     public const int MAX_HRS_IN_MONTH = 10;
-    static void Main(string[] args)
+    public static int computeEmpWage()
     {
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-       
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
             totalWorkingDays++;
             Random random = new Random();
@@ -28,11 +28,16 @@ class Program
                     empHrs = 0;
                     break;
             }
-            
+
             totalEmpHrs += empHrs;
             Console.WriteLine("Day# : " + totalWorkingDays + " Emp Hrs:" + empHrs);
         }
         int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Emp Wage : " + totalEmpwage);
+        return totalEmpwage;
+    }
+    static void Main(string[] args)
+    {
+        computeEmpWage();
     }
 }
