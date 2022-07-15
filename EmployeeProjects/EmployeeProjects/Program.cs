dc6c088509795074fb3263b5cdcb19c1291f287b
srevanwar09 <srevanwar09@domain.com>
2022-07-15 18:17:51 +0530

    Program fole added in uc-6

--- EmployeeProjects/EmployeeProjects/Program.cs
+++ EmployeeProjects/EmployeeProjects/Program.cs
@@ -3,14 +3,15 @@ class Program
     public const int IS_PART_TIME = 1;
     public const int Is_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
-    public const int  NUM_OF_WORKING_DAYS= 2;
+    public const int  NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
     static void Main(string[] args)
     {
-        int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage=0;
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+       
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
@@ -27,10 +28,11 @@ class Program
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage : " + empWage);
+            
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day# : " + totalWorkingDays + " Emp Hrs:" + empHrs);
         }
-        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Total Emp Wage : " + totalEmpwage);
     }
 }
